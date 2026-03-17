2fbc5bb29536b827a123969e25e33bd2dfc15315
Teja Karlapudi <teja2495@gmail.com>
2026-03-17 11:24:45 -0400

    feat: update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.tk.quicksearch"
         minSdk = 24
         targetSdk = 36
-        versionCode = 39
+        versionCode = 40
         versionName = "2.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
